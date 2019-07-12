f4142379614b840b6bd8b5e7197d2cdba73b39fd
Simon Seto <simonseto611@hotmail.com>
2019-07-12 09:25:43 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./image1.jpg",
+  image2: "./image2.jpg",
+  speedIn: 2,
+  speedOut: 4,
+
+  displacementImage: "./dist.png"
+});
+
+// https://github.com/robin-dela/hover-effect
